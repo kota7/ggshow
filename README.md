ac6232dd62609742d1ddd6ae11568f3ea815e00a
Kota Mori <kmori05@gmail.com>
2021-09-15 07:54:31 +0900

    add gg magic command

--- README.md
+++ README.md
@@ -20,11 +20,11 @@ pip install -U ./ggshow
 - Python 3.5+
 - `Rscript` command and R environment with `ggplot2` package installed
 
-## Functions
+## Functionalities
 
 - `ggshow`: Draw graph using ggplot2 on R and return the Image object that you can show on Jupyter notebook
 - `ggwrite`: Draw graph using ggplot2 on R and save it to a file.
-
+- `%gg`, `%%gg`: ipython magic for drawing ggplot2 graph.
 
 
 ```python
@@ -40,14 +40,14 @@ ggshow("""
   x <- c(1,2,3)
   y <- c(4,5,6)
   qplot(x, y, geom="line")
-""", width=3, height=2)
+""", savesize=(3, 2))
 ```
 
 
 
 
     
-<img src="README_files/README_3_0.png" width=400>
+<img width=400 src="README_files/examples_2_0.png">
     
 
 
@@ -70,24 +70,26 @@ ggshow("""
     geom_line(color="blue") +
     geom_line(data=b, linetype="dashed", color="red") +
     theme_bw()
-""", dispwidth=400, width=4, height=2, a=df1, b=df2)
+""", dispwidth=400, savesize=(4, 2), a=df1, b=df2)
 ```
 
 
 
 
     
-<img src="README_files/README_4_0.png" width=400>
+<img width=400 src="README_files/examples_3_0.png">
+    
+
 
 
 
 ```python
 # Example to save the graph to a file
 ggwrite("""
-  x <- c(1,2,3)
-  y <- c(4,5,6)
-  qplot(x, y, geom="line")
-""", "foo.jpg", width=4, height=2)
+    x <- c(1,2,3)
+    y <- c(4,5,6)
+    qplot(x, y, geom="line")
+""", "foo.jpg", savesize=(4, 2))
 ```
 
 
@@ -100,15 +102,125 @@ Image("foo.jpg", width=400)
 
 
     
-<img src="README_files/README_6_0.jpg" width=400>
+<img width=400 src="README_files/examples_5_0.jpg">
+    
 
 
 
 
 ```python
+# Example using %gg magic
+%load_ext ggshow
+```
+
+
+```python
+## line magic
+%gg qplot(1, 2) -s 4 3 --dispwidth 300
+```
+
+
+
+
+    
+<img width=400 src="README_files/examples_7_0.png">
+    
+
+
+
+
+```python
+## line magic with the code as a string variable
+x = np.linspace(-20, 20, 500)
+y = np.sin(x)
+y2 = 0.4*np.cos(x)
+
+df1 = pd.DataFrame({"x":x, "y":y})
+df2 = pd.DataFrame({"x":x, "y":y2})
+code = """
+ggplot(a, aes(x, y)) + 
+  geom_line(color="blue") +
+  geom_line(data=b, linetype="dashed", color="red") +
+  theme_bw() +
+  ggtitle("Sin and cos waves")
+"""
+
+%gg {code} -s 4 2 --dispwidth 400 --data a=df1 b=df2
+```
+
+
+
+
+    
+<img width=400 src="README_files/examples_8_0.png">
+    
+
+
+
+
+```python
+%%gg -s 4 2 --dispwidth 400 --data a=df1 b=df2
+
+## cell magic
+ggplot(a, aes(x, y)) + 
+  geom_line(color="blue") +
+  geom_line(data=b, linetype="dashed", color="red") +
+  theme_bw() +
+  ggtitle("Sin and cos waves")
+```
+
+
+
+
+    
+<img width=400 src="README_files/examples_9_0.png">
+    
+
+
+
+
+```python
+# show the full command options
+%gg --help
+```
+
+    Help on method gg in module ggshow:
+    
+    gg(line, cell=None) method of ggshow.GGMagic instance
+        ::
+        
+          %gg [--help] [-s SAVESIZE SAVESIZE] [--scale SCALE] [--units UNITS]
+                  [--dpi DPI] [-w DISPWIDTH] [-h DISPHEIGHT]
+                  [--libs [LIBS [LIBS ...]]] [--data [DATA [DATA ...]]]
+                  [plotcode [plotcode ...]]
+        
+        positional arguments:
+          plotcode              R code
+        
+        optional arguments:
+          --help
+          -s <SAVESIZE SAVESIZE>, --savesize <SAVESIZE SAVESIZE>
+                                height, width
+          --scale SCALE         ggsave option scale
+          --units UNITS         ggsave option units
+          --dpi DPI             ggsave option dpi
+          -w DISPWIDTH, --dispwidth DISPWIDTH
+                                display width
+          -h DISPHEIGHT, --dispheight DISPHEIGHT
+                                display width
+          --libs <[LIBS [LIBS ...]]>
+                                R libraries to use
+          --data <[DATA [DATA ...]]>
+                                data frames mapping as {name in r}={name in python}
+    
+
+
+
+```python
 # By default, we use 'Rscript' as the command to run R code.
-# If this is not a valid command name on your environment,
-# specify the right one by set_rscript function.
+# If this is not a valid command on the environment,
+# we can still use the functions by
+# specifying the right one using set_rscript function.
 from ggshow import config, set_rscript
 
 set_rscript("/usr/bin/Rscript")  
@@ -118,6 +230,4 @@ print(config.rscript)
 ```
 
     /usr/bin/Rscript
-
-
 
--- ggshow.py
+++ ggshow.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import subprocess
+import warnings
 from tempfile import TemporaryDirectory
+
+try:
+    # This try..except avoids errors on an environment with no ipython
+    # ggwrite function will still be available
+    from IPython.display import Image
+except ImportError as e:
+    print("'ggshow' function is disabled. Please import ipython to use it", file=sys.stderr)
 
 class config:
     rscript = "Rscript"
@@ -24,19 +33,41 @@ def _find_rscript()-> bool:
 if not _find_rscript():
     print("'{}' is not a valid Rscript command. Please set by set_rscript(<rscript command>)")
 
-def ggwrite(plotcode: str, outfile: str, libs=(),
-            width=None, height=None, scale=1, units="in", dpi=300,
-            ggsave_opts: dict={}, **dataframes):
+def ggwrite(plotcode: str, outfile: str, libs: tuple=(),
+            savesize: tuple=None, width: float=None, height: float=None,
+            scale: float=1, units: str="in", dpi: int=300, **data):
+    """
+    Write a ggplot2 graph to a file
+
+    args:
+        plotcode :   R script to make a plot.
+        outfile  :   The graph is saved to this filepath
+        libs     :   A Sequence libraries to use inside the R script
+        savesize :   Graph size (width, height) to save
+        width    :   Another way to specify savesize[0]
+        height   :   Another way to specify savesize[1]
+        scale    :   ggsave option scale
+        units    :   ggsave option units
+        dpi      :   ggsave option gpi
+        **data   :   pandas data frames with names used inside the R script
+
+    Returns:
+        None
+    """
     with TemporaryDirectory() as tmpdir:
+        libs = tuple(libs)
         libs += ("ggplot2",)
         importcode = ";".join("library({})".format(l) for l in libs)
         readcode = []
-        for name, df in dataframes.items():
+        for name, df in data.items():
              filename = os.path.join(tmpdir, "__data_{}.csv".format(name))
              df.to_csv(filename, index=False)
              readcode.append("{} <- read.csv('{}', as.is=TRUE)".format(name, filename))
         readcode = ";".join(readcode)
 
+        if savesize is None: savesize = None, None
+        if width is None: width = savesize[0]
+        if height is None: height = savesize[1]
         if width is None: width = "NA"
         if height is None: height = "NA"
         code = """
@@ -52,14 +83,86 @@ def ggwrite(plotcode: str, outfile: str, libs=(),
         subprocess.run([config.rscript, "-e", code])
 
 
-def ggshow(plotcode: str, dispwidth=300, dispheight=None, libs=(),
-           width=None, height=None, scale=1, units="in", dpi=300,
-           ggsave_opts: dict={}, **dataframes)-> Image:
-    from IPython.display import Image
+def ggshow(plotcode: str, dispwidth: float=300, dispheight: float=None, libs: tuple=(),
+           savesize: tuple=None, width: float=None, height: float=None, scale: float=1, units: str="in", dpi: int=300,
+           **data)-> Image:
+    """
+    Draw a ggplot2 graph
+
+    args:
+        plotcode :   R script to make a plot.
+        libs     :   A Sequence libraries to use inside the R script
+        savesize :   Graph size (width, height) to save
+        width    :   Another way to specify savesize[0]
+        height   :   Another way to specify savesize[1]
+        scale    :   ggsave option scale
+        units    :   ggsave option units
+        dpi      :   ggsave option gpi
+        **data   :   pandas data frames with names used inside the R script
+
+    Returns:
+        IPython.core.Image
+    """
     with TemporaryDirectory() as tmpdir:
         outfile = os.path.join(tmpdir, "__ggout.png")
         ggwrite(plotcode, outfile, libs=libs,
-                width=width, height=height, scale=scale, units=units, dpi=dpi,
-                ggsave_opts=ggsave_opts, **dataframes)
+                savesize=savesize, width=width, height=height, scale=scale, units=units, dpi=dpi,
+                **data)
         im = Image(filename=outfile, width=dispwidth, height=dispheight)
         return im
+
+
+try:
+    # We enclose this whole section by try...except
+    # so that the module can be imported on an environment with no ipython
+    from IPython.core.magic import Magics, line_cell_magic, magics_class
+    from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
+
+    @magics_class
+    class GGMagic(Magics):
+
+        @line_cell_magic("gg")
+        @magic_arguments()
+        @argument("plotcode", type=str, nargs="*", help="R code")
+        @argument("--help", action="store_true")
+        @argument("-s", "--savesize", type=float, nargs=2, default=None, help="height, width")
+        @argument("--scale", type=float, default=1, help="ggsave option scale")
+        @argument("--units", type=str, default="in", help="ggsave option units")
+        @argument("--dpi", type=int, default=300, help="ggsave option dpi")
+        @argument("-w", "--dispwidth", type=float, default=None, help="display width")
+        @argument("-h", "--dispheight", type=float, default=None, help="display width")
+        @argument("--libs", nargs="*", default=(), help="R libraries to use")
+        @argument("--data", nargs="*", default=(), help="data frames mapping as {name in r}={name in python}")
+        def gg(self, line, cell=None):
+            args = parse_argstring(self.gg, line)
+            if args.help:
+                help(self.gg)
+                return
+            opts = vars(args)
+            del opts["help"]
+            #print(args)
+            #print(opts)
+            for d in opts.pop("data"):
+                rname, pyname = d.split("=")
+                rname = rname.strip()
+                pyname = pyname.strip()
+                assert pyname in self.shell.user_ns, "'{}' not found in the name space:\n{}".format(pyname, self.shell.user_ns)
+                opts[rname] = self.shell.user_ns[pyname]
+            opts["plotcode"] = " ".join(opts["plotcode"])
+
+            if cell is not None:
+                opts["plotcode"] = cell
+            
+            return ggshow(**opts)
+            
+    def load_ipython_extension(ipython):
+        """
+        Any module file that define a function named `load_ipython_extension`
+        can be loaded via `%load_ext module.path` or be configured to be
+        autoloaded by IPython at startup time.
+        """
+        # You can register the class itself without instantiating it.  IPython will
+        # call the default constructor on it.
+        ipython.register_magics(GGMagic)
+except ImportError as e:
+    pass
